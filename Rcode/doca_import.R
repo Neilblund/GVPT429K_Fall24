60e7601911608245bdfc3a07eddb6fb11f562950
Neilblund <neilblund@gmail.com>
2024-10-01 13:05:43 -0400

    fix

--- Rcode/doca_import.R
+++ Rcode/doca_import.R
@@ -1,8 +1,8 @@
-required_packages<-c("haven")
-for(p in required_packages){
-  if(!require(p,character.only = TRUE)) install.packages(p)
-  
+
+if(!("haven" %in% installed.packages())){
+  install.packages('haven')
 }
+
 
 if(!file.exists('final_data_v10.dta')){
   download.file('http://www.stanford.edu/group/collectiveaction/final_data_v10.dta', 
@@ -12,4 +12,5 @@ if(!file.exists('final_data_v10.dta')){
   
 }
 
-doca<-read_dta('final_data_v10.dta', encoding='latin1')
+doca<-haven::read_dta('final_data_v10.dta', encoding='latin1')
+
